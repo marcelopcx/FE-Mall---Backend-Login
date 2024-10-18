428514be072df5e3756a29f7110473764d4fb499
marcelopcx <marceloperozocx@gmail.com>
2024-10-18 11:28:44 -0400

    feat: inicialización del proyecto.

---
+++ src/main/java/com/marcelopcx/fe_mall_backend_login/FeMallBackendLoginApplication.java
@@ -0,0 +1,13 @@
+package com.marcelopcx.fe_mall_backend_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeMallBackendLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeMallBackendLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelopcx/fe_mall_backend_login/FeMallBackendLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelopcx.fe_mall_backend_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeMallBackendLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
